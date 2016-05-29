194e8b5b2744636c37a53339f17c1e5f70ec67fd
Mathieu Renault <mathieu.renault@telecom-paristech.fr>
2016-05-29 16:08:33 +0200

    La génération de données manquantes fonctionne désormais avec n'importe quel fichier csv.

--- Data processing/js/processing.js
+++ Data processing/js/processing.js
@@ -8,53 +8,52 @@ var file = "data/flowers.csv"; //Path of the file to process
 var svg;
 var dataset = [];
 var datasetMV = [];
-			
-d3.csv(file)
-	.row(function (d, i) {
-		return {
-			sepalLength: +d["sepal length"],
-			sepalWidth: +d["sepal width"],
-			petalLength: +d["petal length"],
-			petalWidth: +d["petal width"],
-			species: d["species"]
+var keysArray = [];
+
+d3.csv(file, function(data) {
+	
+	data.forEach(function (data, accessor) {
+		var row;
+		row = {
+			id: accessor
 		};
-	})
-	.get(function(error, rows) {
-		console.log("Loaded " + rows.length + " rows");
-		
-		if (rows.length > 0) {
-			dataset = rows; //Store the data
-			svg = d3.select("body")
-					.append("svg")
-					.attr("width", Object.keys(rows[0]).length * w)
-					.attr("height", (rows.length * h) + 40);
-			generateMissingValue(dataset);
-			display(datasetMV);
+		for(var attr in data) {
+			if(data.hasOwnProperty (attr)) {
+				row[attr] = data[attr];
+			}
 		}
-		
+		dataset.push(row);
 	});
 	
+	for (var attr in dataset[0]) {
+		keysArray.push(attr);
+	}
+	
+	console.log("Loaded " + dataset.length + " rows");
+	
+	if (dataset.length > 0) {
+			svg = d3.select("body")
+					.append("svg")
+					.attr("width", Object.keys(dataset[0]).length * w)
+					.attr("height", (dataset.length * h) + 40);
+			generateMissingValue(dataset);
+			console.log(datasetMV);
+			display(datasetMV);
+		}
+	
+});
+
+	
 function generateMissingValue(data) {
+	
 	datasetMV = data;
+	
 	for (var x = 0; x < data.length; x++) {
+		
 		if ((Math.floor(Math.random() * 100) + 1)  > (100 - rate)) {
-			switch (Math.floor(Math.random() * 5) + 1) { //Choose a column randomly
-				case 1:
-					datasetMV[x].sepalLength = "NaN";
-					break;
-				case 2:
-					datasetMV[x].sepalWidth = "NaN";
-					break;
-				case 3:
-					datasetMV[x].petalLength = "NaN";
-					break;
-				case 4:
-					datasetMV[x].petalWidth = "NaN";
-					break;
-				case 5:
-					datasetMV[x].species = "NaN";
-					break;
-			}
+
+			datasetMV[x][keysArray[Math.floor(Math.random() * (keysArray.length - 1)) + 1]] = "NaN";
+			
 		}
 	}
 }
@@ -64,7 +63,13 @@ function display(data) {
         .data(data)
         .enter()
         .append("text")
-        .text( function(d,i,j) { return (d.sepalLength + " | " + d.sepalWidth + " | " + d.petalLength + " | " + d.petalWidth + " | " + d.species); } )
+        .text( function(d,i,j) {
+			var textResult = "| ";
+			for (var k = 1; k < keysArray.length; k++) { //On n'affiche pas l'id
+				textResult += d[keysArray[k]] + " | ";
+			}
+			return (textResult);
+		})
         .attr("x", 0)
         .attr("y", function(d,i,j) { return (i * 20) + 40; })
         .attr("font-family", "sans-serif")
